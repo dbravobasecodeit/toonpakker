dbd12314c5f5c8c067e6e810d0c76aa7f49e014c
Dario Bravo <dbravo@basecodeit.com>
2019-06-28 13:46:32 -0300

    Bla

---
+++ CartoonAssault/Assets/Cartoon Assault/CameraMovementUnaCosaAsi.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovementUnaCosaAsi : MonoBehaviour
+{
+    public Transform playerCar;
+    public float cameraDistance = 30.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate() {
+        transform.position = new Vector3(playerCar.position.x, 0, -9);
+    }
+}
